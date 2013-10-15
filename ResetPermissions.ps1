21fba1428ca53dd430f16c4857a8bca9e11627c5
Brandon Penglase <bp-gh@spaceservices.net>
2013-10-15 10:52:01 -0400

    Upload of ResetPermissions
    This is a quick (well not really) hack to reset all permissions on
    folders on the fileserver. It is very dirty, but does work. It also
    takes a long time on a lot of folders (2+ hours on ~250gig of teacher
    data).
    Basically run it as the domain Administrator account on the fileserver,
    it takes ownership, makes sure inheritance is enabled, verifies the user
    will have access, then sets ownership back to the user.
    This relies on the folder name being the same as the user account in AD.
    This fails if the user account was renamed, but the folder was not (e.x.
    previous admin had changed some accounts of teacher who got marriade,
    but did not change the homedirectory name. They were unable to get their
    documents. I fixed this by renaming the folder, and updating the
    homedirectory path in AD).

---
+++ ResetPermissions.ps1
@@ -0,0 +1,42 @@
+﻿$serverpath = "E:\TeacherData\UES"
+foreach ($user in Get-ChildItem $serverpath) {
+    $homedir = $serverpath+"\"+$user
+    # Try to see if we can read the ACL (if we can, then we can continue, if not, we need to take ownership
+    try {
+        takeown /R /D Y /F $homedir
+        icacls $homedir /reset /t /c /q
+        icacls $homedir /setowner "example\administrator" /t /c /q
+        # Once this is ran, see if it was successful by getting the ACL List. If not, something else is messed up, so exit
+        try {
+            $acl = Get-Acl $homedir
+            }
+        catch {
+            write-host Failed to take ownership
+            sys.exit
+        }
+    }
+    Catch {
+        write-host Failed to take ownership
+        sys.exit
+    }
+$rights=[System.Security.AccessControl.FileSystemRights]::FullControl
+$inheritance=[System.Security.AccessControl.InheritanceFlags]"ContainerInherit,ObjectInherit"
+$propagation=[System.Security.AccessControl.PropagationFlags]::InheritOnly
+$allowdeny=[System.Security.AccessControl.AccessControlType]::Allow
+$dirace=New-Object System.Security.AccessControl.FileSystemAccessRule ($user,$rights,$inheritance,$propagation,$allowdeny)
+$ACL.AddAccessRule($dirace)
+$rights=[System.Security.AccessControl.FileSystemRights]::FullControl
+$inheritance=[System.Security.AccessControl.InheritanceFlags]"ContainerInherit,ObjectInherit"
+$propagation=[System.Security.AccessControl.PropagationFlags]::None
+$allowdeny=[System.Security.AccessControl.AccessControlType]::Allow
+$dirace=New-Object System.Security.AccessControl.FileSystemAccessRule ($user,$rights,$inheritance,$propagation,$allowdeny)
+$ACL.AddAccessRule($dirace)
+Set-Acl $homedir $acl
+    # Now do the same thing for every subfolder
+    Get-ChildItem $homedir | foreach {        
+        icacls $homedir"\"$_ /t /c /q /reset
+        icacls $homedir"\"$_ /setowner $user /t /c /q
+    }
+    # Reset Ownership back to the person's whose home directory this is
+    icacls $homedir /setowner $user /t /c /q
+}
