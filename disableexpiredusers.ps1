e7298b6e8732f3c74ad935eff11366831d3f1130
Brandon Penglase <bp-gh@spaceservices.net>
2013-10-15 10:07:55 -0400

    Naming Change
    Changing domain to example.

--- disableexpiredusers.ps1
+++ disableexpiredusers.ps1
@@ -4,10 +4,10 @@
 #	http://www.tomsitpro.com/articles/active_directory-powershell-windows_server-aduc-scripting,2-250.html
 #	https://mikegriffin.ie/blog/20120521-filtering-with-powershell-and-get-adcomputer/
 
-$RootDomain="DC=nhsd,DC=org"
+$RootDomain="DC=example,DC=org"
 $CutOff = (Get-Date).AddDays(0)
-$UsersDisables = "OU=LES,OU=Disabled,$RootDomain"
-$UsersBase = "OU=LES,OU=Students,$RootDomain"
+$UsersDisables = "OU=School,OU=Disabled,$RootDomain"
+$UsersBase = "OU=School,OU=Students,$RootDomain"
 $Users = Get-ADUser -SearchBase $UsersBase -filter { AccountExpirationDate -lt $Today }
 ForEach ($User1 in $Users) {
 	if ($User1 -ne $null) {
