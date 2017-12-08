a1c4424e06b7ba297ac4d8ed302e87ba1938bc3f
Brandon Penglase <bp-gh@spaceservices.net>
2017-12-08 10:11:21 -0500

    Add OSDCompName
    Used to set a variable in SCCM, based on the computer name.

---
+++ OSDCompName.ps1
@@ -0,0 +1,27 @@
+﻿Try {
+    $tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
+    $ComputerName=$tsenv.Value("OSDComputerName")
+    if ($ComputerName -like "10-*") {
+        $tsenv.Value("OSDComputerName")=$ComputerName.Replace("10-",'')
+        $tsenv.Value("NHSDWinVer")="10"
+    } elseif ($ComputerName -like "12-*") {
+        $tsenv.Value("OSDComputerName")=$ComputerName.Replace("12-",'')
+        $tsenv.Value("NHSDWinVer")="12"
+    } elseif ($ComputerName -like "8-*") {
+        $tsenv.Value("OSDComputerName")=$ComputerName.Replace("8-",'')
+        $tsenv.Value("NHSDWinVer")="8"
+    } elseif ($ComputerName -like "16-*") {
+        $tsenv.Value("OSDComputerName")=$ComputerName.Replace("16-",'')
+        $tsenv.Value("NHSDWinVer")="16"
+    } elseif ($ComputerName -like "7-*") {
+		# If for some reason 7 is still requested.. run with 8 instead...
+		$tsenv.Value("OSDComputerName")=$ComputerName.Replace("7-",'')
+		$tsenv.Value("NHSDWinVer")="8"
+	}else {
+        $tsenv.Value("NHSDWinVer") = "10"
+    }
+}
+Catch {
+    Write-Host "This only runs in a Task Sequence"
+    echo "Failure" |  Out-File C:\ps.log 
+}
